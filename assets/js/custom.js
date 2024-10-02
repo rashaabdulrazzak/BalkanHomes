24fc72bb40933621e0c5f9f5b771efc913fcd8c8
rashaabdulrazzak <rasha.abdulrazzak.com>
2024-10-02 18:32:28 +0300

    make some change like icon and style

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,7 +2,7 @@
   "use strict";
 
   // Page loading animation
-  /* $(window).on("load", function () {
+  /*  $(window).on("load", function () {
     $("#js-preloader").addClass("loaded");
   });*/
 
